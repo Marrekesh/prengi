77ac4f0dd04d776ea7526608158ca09c9d5156e6
Dima <warreaver123@gmail.com>
2021-03-25 23:40:54 +0200

    slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,4 +10,14 @@ $(document).ready(function (){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
     });
+
+    // $(window).ready(function(){
+    //     // Установка высоты блока при загрузке страницы
+    //     $('.promo__carousel-img').css('height', $(this).width() / 3 )
+    //     $('.console').html('100%: ' + $(this).width() + '<br>500px: ' + $(this).width() / 3)
+    // }).resize(function(){ 
+    //     // Установка высоты блока при изменении размеров окна
+    //     $('.promo__carousel-img').css('height', $(this).width() / 3 )
+    //     $('.console').html('100%: ' + $(this).width() + '<br>500px: ' + $(this).width() / 3)
+    // })
 })
