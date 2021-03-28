d2445f6e2e6ff6ee9204967a43579951fc289eb7
Dima <warreaver123@gmail.com>
2021-03-28 15:39:20 +0300

    variant

--- src/js/script.js
+++ src/js/script.js
@@ -4,22 +4,10 @@ $(document).ready(function (){
         infinite: true,
         speed: 1200,
         slidesToShow: 1,
-        // adaptiveHeight: true,
         slidesToScroll: 1,
-        // variableWidth: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
     });
-
-    // $(window).ready(function(){
-    //     // Установка высоты блока при загрузке страницы
-    //     $('.promo__carousel-img').css('height', $(this).width() / 3 )
-    //     $('.console').html('100%: ' + $(this).width() + '<br>500px: ' + $(this).width() / 3)
-    // }).resize(function(){ 
-    //     // Установка высоты блока при изменении размеров окна
-    //     $('.promo__carousel-img').css('height', $(this).width() / 3 )
-    //     $('.console').html('100%: ' + $(this).width() + '<br>500px: ' + $(this).width() / 3)
-    // })
 
     //Solution carousel
 
@@ -41,7 +29,7 @@ $(document).ready(function (){
         $(this)
           .addClass('solutions__tab_active').siblings().removeClass('solutions__tab_active')
           .closest('div.container').find('div.solutions__tab-content').removeClass('solutions__tab-content_active').eq($(this).index()).addClass('solutions__tab-content_active');
-          $('.solutions__carousel').slick("reinit");
+        //   $('.solutions__carousel').slick("reinit");
       });
 
 })
